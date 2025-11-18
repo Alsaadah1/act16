4fb02cfcc00782cd812c06b877e5a135fe8f32ef
Alsaadah <alsaadahalabri@gmail.com>
2025-11-18 08:55:26 +0400

    3rd commit

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div>
-      <b>Anthony - Lecturer 2025-2026</b>
+      <b>Anthony - Lecturer semester1 2025-2026</b>
     </div>
   );
 };
--- client/src/Features/UserSlice.js
+++ client/src/Features/UserSlice.js
@@ -60,7 +60,7 @@ export const login = createAsyncThunk("users/login", async (userData) => {
 export const logout = createAsyncThunk("/users/logout", async () => {
   try {
     // Send a request to your server to log the user out
-    const response = await axios.post(`${ENV.SERVER_URL}/logout`);
+    const response = await axios.post("http://localhost:3001/logout");
   } catch (error) {}
 });
 
